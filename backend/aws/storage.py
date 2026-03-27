b6c4557cf9482f66e3580a9c2d5be3e3f5a20f83
Momtez Mdimagh <momtez.mdimagh@insat.ucar.tn>
2026-03-27 18:21:13 +0100

    Refactor save_metrics and save_logs functions to create directories and use timestamped filenames for better organization

--- backend/aws/storage.py
+++ backend/aws/storage.py
@@ -1,17 +1,44 @@
 from typing import List
 from pathlib import Path
 import json
+
 from schemas.metrics import Metrics
 from schemas.log_event import LogEvent
 
+
 def save_metrics(metrics: Metrics):
-    with open("metrics.json", "w") as f:
+    # create folder if not exists
+    path = Path("./data/metrics")
+    path.mkdir(parents=True, exist_ok=True)
+
+    # create unique filename using timestamp + instance_id
+    timestamp = metrics.timestamp.isoformat().replace(":", "-")
+    filename = f"{metrics.instance_id}_{timestamp}.json"
+
+    file_path = path / filename
+
+    with file_path.open("w", encoding="utf-8") as f:
         json.dump(metrics.model_dump(mode="json"), f, indent=2)
 
+    print(f"Metrics saved to {file_path}")
+
 def save_logs(logs: List[LogEvent]):
-    with open("logs.json", "w") as f:
+    if not logs:
+        return
+
+    path = Path("./data/logs")
+    path.mkdir(parents=True, exist_ok=True)
+
+    timestamp = logs[0].timestamp.isoformat().replace(":", "-")
+    filename = f"{logs[0].instance_id}_{timestamp}.json"
+
+    file_path = path / filename
+
+    with file_path.open("w", encoding="utf-8") as f:
         json.dump(
             [log.model_dump(mode="json") for log in logs],
             f,
             indent=2
         )
+
+    print(f"Logs saved to {file_path}")
